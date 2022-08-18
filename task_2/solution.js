3f74fff482036547a8377cfd8c75a9d3f8ae6742
Alexey Dudov <lesha-dudov@list.ru>
2022-08-18 13:23:01 +0300

    Task 2.1 Done!

--- task_2/solution.js
+++ task_2/solution.js
@@ -2,33 +2,16 @@ function calcShipping(sum, min, shipping,) {
     let productsSum = sum; // сумма в корзине
     let freeShippingMinSum = min; // минимальная цена для бесплатной доставки
     let shippingPrice = shipping; // стоимость доставки
+
+    // Задание №2.1. Рассчитать доставку
+
     let shippingSum;
 
     if(productsSum == 0 || productsSum >= freeShippingMinSum) {
         shippingSum = 0
-    }
-    
-    else if (productsSum > 0 && productsSum < freeShippingMinSum) {
+    }else if (productsSum > 0 && productsSum < freeShippingMinSum) {
         shippingSum = shippingPrice
     }
-    
-    
-        
-
-    // Задание №2.1. Рассчитать доставку
-
-    // создайте переменную shippingSum
-
-    // если productsSum равно 0,
-    // то shippingSum присвоить значение 0
-
-    // если productsSum Больше или равна freeShippingMinSum,
-    // то shippingSum присвоить значение 0
-
-    // если productsSum больше 0 и меньше freeShippingMinSum,
-    // то shippingSum присвоить значение shippingPrice
-
-    // Конец решения задания №2.1.
 
     return shippingSum;
 }
